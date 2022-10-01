7e59d4860359d7b5a44213872cfd5e8c469db2f1
Colby-Coffman <114829458+Colby-Coffman@users.noreply.github.com>
2022-10-01 18:56:24 -0500

    Update viewer.py

--- viewer.py
+++ viewer.py
@@ -1,11 +1,10 @@
-  1 import matplotlib.pyplot as plt
-  2 import pydicom
-  3 import pydicom.data
-  4 
-  5 def dcm_viewer(base, file):
-  6         base = r"{}".format(base)
-  7         filename = pydicom.data.data_manager.get_files(base, file)[0]
-  8         ds = pydicom.dcmread(filename)
-  9         plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
- 10         plt.show()
- 11         return
+import matplotlib.pyplot as plt
+import pydicom.data
+
+def dcm_viewer(base, file):
+  base = r"{}".format(base)
+  filename = pydicom.data.data_manager.get_files(base, file)[0]
+  ds = pydicom.dcmread(filename)
+  plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
+  plt.show()
+  return
