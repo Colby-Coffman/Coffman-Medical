d0074b9847af87adce232dac5281d1e0b3b4fcf0
Colby-Coffman <114829458+Colby-Coffman@users.noreply.github.com>
2022-10-07 17:58:30 -0500

    Update dcm.py

--- dcm.py
+++ dcm.py
@@ -4,19 +4,23 @@ import os
 import tkinter as tk
 import pathlib
 
-# Relative path is case insensitive! (Could cause errors if there are more than one file of the same name in the current working directory)
-# No error for no file specified
 def viewer(path: str):
 	path_components = os.path.split(path)
 	if path_components[1] == "":
+		assert False, "No File ERROR not implemented"
 		return
 	if path_components[0] == "":
 		base = os.getcwd()
 		current_directory = pathlib.Path(".")
+		found_match = False
 		for current_file in current_directory.iterdir():
 			if str(current_file).upper() == path.upper():
 				file = str(current_file)
+				found_math = True
 				break
+		if found_match != True:
+			assert False, "Relative File Not Found ERROR not implemented"
+			return
 	else:
 		base = path_components[0]
 		file = path_components[1]
@@ -36,7 +40,6 @@ def viewer_gui():
 	window.mainloop()
 	return
 
-# Private helper function (I don't know how to write private functions in python lol)
 def _on_change(window, entry):
 	path = entry.get()
 	window.destroy()
