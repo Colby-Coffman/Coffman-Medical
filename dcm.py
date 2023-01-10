99ec3d76ee27a06bc63d1bde65438a3a4ce708fc
Colby-Coffman <114829458+Colby-Coffman@users.noreply.github.com>
2023-01-10 17:34:46 -0600

    Update dcm.py

--- dcm.py
+++ dcm.py
@@ -3,10 +3,10 @@ import pydicom.data
 import os
 import tkinter as tk
 import pathlib
+import numpy as np
+from skimage import img_as_ubyte, exposure
 
 def viewer(path: str):
-	if os.path.exists(path) == False:
-		assert False, "No Non-Existent File ERROR implemented"
 	path_components = os.path.split(path)
 	if path_components[1] == "":
 		assert False, "No File ERROR not implemented"
@@ -28,16 +28,33 @@ def viewer(path: str):
 		file = path_components[1]
 	filename = pydicom.data.data_manager.get_files(base, file)[0]
 	ds = pydicom.dcmread(filename)
-	plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
+	arr = np.uint8((ds.pixel_array / ds.pixel_array.max()) * 255)
+	pixel_subtract = 220
+	for array in arr:
+		for i in range(len(array)):
+			if array[i] < pixel_subtract:
+				array[i] = 0
+				continue
+			array[i] = array[i] - pixel_subtract
+	plt.imshow(arr, cmap='gray')
 	plt.show()
+	col_row_string = ' '.join(reversed([str(x) for x in arr.shape]))
+	pgmheader = '\n'.join(('P5', col_row_string, str(arr.max())))
+	pgmheader += '\n'
+	with open('MRImage.pgm', 'wb') as file:
+		file.write(pgmheader.encode('ascii'))
+		file.write(arr.tobytes())
 	return
 
 def viewer_gui():
 	window = tk.Tk()
+	window.title("Coffman Medical")
+	photo = tk.PhotoImage(file="icon.png")
+	window.iconphoto(False, photo)
 	prompt = tk.Label(window, text="Please enter the absolute or relative path of the .dcm file", foreground = "white", background = "black", width = 50, height = 10)
 	entry = tk.Entry(window, width = 50)
-	prompt.pack()
-	entry.pack()
+	prompt.grid(row=0,column=0)
+	entry.grid(row=1, column=0)
 	entry.bind("<Return>", lambda command: _on_change(window, entry))
 	window.mainloop()
 	return
